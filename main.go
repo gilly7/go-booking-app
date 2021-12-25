6c4026764e6d0483e921c1db3bf5ddb82fe7ff69
Gilbert Ngeno <ngenogilbert07@gmail.com>
2021-12-25 20:40:51 +0300

    Enter suer input

--- main.go
+++ main.go
@@ -3,12 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	var conferenceName string = "Go Conference"
 
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 	fmt.Println("Welcome to ", conferenceName, "booking application")
 	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend")
 
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
+	//ask user for their name
+	fmt.Println("Enter your First Name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your Last Name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
 }
