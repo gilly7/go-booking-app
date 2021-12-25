d673530adb7a97fd3e276e8e889439387c8ad427
Gilbert Ngeno <ngenogilbert07@gmail.com>
2021-12-25 21:06:12 +0300

    arrays

--- main.go
+++ main.go
@@ -6,17 +6,19 @@ func main() {
 	var conferenceName string = "Go Conference"
 
 	const conferenceTickets int = 50
-	var remainingTickets int = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 	fmt.Println("Welcome to ", conferenceName, "booking application")
 	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend")
 
+	var bookings [50]string
+
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	//ask user for their name
 	fmt.Println("Enter your First Name: ")
@@ -31,5 +33,16 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array:  %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array Type: %T\n", bookings)
+	fmt.Printf("Array Length:  %v\n", len(bookings))
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
+
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
 }
