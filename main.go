1236916bca79e4e62a20e3a2a67ec142375cc5a8
Gilbert Ngeno <ngenogilbert07@gmail.com>
2021-12-28 21:41:49 +0300

    further user input validation

--- main.go
+++ main.go
@@ -41,9 +41,9 @@ func main() {
 		isValidName := len(firstName) >= 2 && len(lastName) >= 2
 		isValidEmail := strings.Contains(email, "@")
 		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-		isValidCity := city != "Singapore" || city == "London"
 
-		if userTickets <= remainingTickets {
+		if isValidName && isValidEmail && isValidTicketNumber {
+
 			remainingTickets = remainingTickets - userTickets
 			//	bookings[0] = firstName + " " + lastName
 
@@ -70,7 +70,7 @@ func main() {
 
 			}
 		} else {
-			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+			fmt.Println("Your input data is invalid, try again!")
 
 		}
 	}
